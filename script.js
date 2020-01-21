802c299904f352005e31e8c1a247c02d9c36b94c
INWYTIA <inwytia@gmail.com>
2020-01-21 17:17:51 -0600

    html header and search bar as well as api url keys

---
+++ script.js
@@ -0,0 +1,13 @@
+//key = cad9eb6e38bd8ec8d96c293c18381b8e
+
+//current weather url = 
+//http://api.openweathermap.org/data/2.5/weather?q= {city name} &units=imperial&APPID=cad9eb6e38bd8ec8d96c293c18381b8e
+// ...weather?id= {city id}
+// ...weather?zip= {us zip code},{optional country code}
+
+//forcast url = 
+//api.openweathermap.org/data/2.5/forecast?q=London  all parameters apply
+
+//uv url = 
+//api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37
+//http://api.openweathermap.org/data/2.5/uvi/forecast?appid={appid}&lat={lat}&lon={lon}&cnt={days} 
