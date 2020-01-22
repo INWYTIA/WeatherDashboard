0af9061a13545851cc16b2d7ae54bd75898c8014
INWYTIA <inwytia@gmail.com>
2020-01-22 03:33:17 -0600

    removed unnecessary comments and consol logs

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//key = cad9eb6e38bd8ec8d96c293c18381b8e
-
-//current weather url = 
-//http://api.openweathermap.org/data/2.5/weather?q= {city name} &units=imperial&APPID=cad9eb6e38bd8ec8d96c293c18381b8e
-// ...weather?id= {city id}
-// ...weather?zip= {us zip code},{optional country code}
-
-//forcast url = 
-//api.openweathermap.org/data/2.5/forecast?q=London  all parameters apply
-
-//uv url = 
-//api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37
-//http://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid={appid}
-
 var prvCities = [];
 var dayMarkers = [0, 8, 16, 24, 32,];
 
@@ -106,17 +92,13 @@ function update (place) {
                 uvDiv.addClass('moderate');
             };
             uvDiv.text(uvIndex);
-            console.log(uvVar);
         });
-        console.log(response);
-        console.log(prvCities);
     });
     $.ajax({
         url : frcstURL,
         method : 'GET',
     }).done(function (forecast) {
         makeForecast(forecast);
-        console.log(forecast);
     });
 }
 
